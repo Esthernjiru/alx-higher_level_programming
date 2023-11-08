dbd0eb41d6cab1549739215c7b686f3640689b3b
Esther njiru <esthernjiru86@gmail.com>
2023-11-08 06:50:24 -0800

    Data structures,set, dict

---
+++ 0-square_matrix_simple.py/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+#7-update_dictionary.py
+
+def update_dictionary(my_dict, key, value):
+    my_dict[key] = value
+    return my_dict
+
---
+++ 0-square_matrix_simple.py/8-simple_delete.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+#8-simple_delete.py
+
+def simple_delete(my_dict, key=""):
+    my_dict.pop(key, None)
+    return my_dict
+
