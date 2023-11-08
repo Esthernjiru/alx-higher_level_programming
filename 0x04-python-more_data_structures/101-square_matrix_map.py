5d768d3f76bed42ffd112052bac87409837ee6ab
Esther njiru <esthernjiru86@gmail.com>
2023-11-08 12:56:25 -0800

    square value int

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -2,5 +2,4 @@
 #101-square_matrix_map.py
 
 def square_matrix_map(matrix=[]):
-    return list(map(lambda row: list(map(lambda col: col**2, row)), matrix))
-
+    return (list(map(lambda x: list(map(lambda y: y ** 2, x[:])), matrix)))
