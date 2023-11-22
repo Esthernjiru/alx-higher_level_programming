80c28628db1807728f2e399f877dc5f8dbcd3d9b
Esther njiru <esthernjiru86@gmail.com>
2023-11-22 14:52:59 -0800

    Classes and Objects

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,9 @@
+##!/usr/bin/python3
+#101-square.py
+
+"""Define a class square"""
+
+ Class square:
+     """Represent a square"""
+     def __init__(self, size=0 position=(0, 0):
+             """initialize a new square.
