849b66806a952799c493be86cce702fade7f3ba3
Esther njiru <esthernjiru86@gmail.com>
2023-12-06 10:09:55 -0800

    Hash tables

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -19,3 +19,4 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 
 	return (hash % size);
 }
+
--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -95,3 +95,4 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	return (1);
 }
+
